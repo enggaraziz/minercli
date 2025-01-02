d0ff34bb5526cdffb453e9da8caedf436cce4c21
Enggar A <cyber4rt000@gmail.com>
2025-01-02 15:01:54 +0700

    Update wallet.py

--- miner/wallet.py
+++ miner/wallet.py
@@ -33,7 +33,8 @@ def get_chain_manager():
     chain_manager = vana.ChainManager(config=config)
     chain_manager.web3 = Web3(Web3.HTTPProvider("https://rpc.vana.org"))
     chain_manager.web3.middleware_onion.inject(geth_poa_middleware, layer=0)
-    return vana.ChainManager()
+    return chain_manager
+    # return vana.ChainManager()
 
 # def get_chain_manager():
 #     config = vana.Config()
